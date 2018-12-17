01130e14c3f8240b4c3b23da560337df61830da6
Guy Avraham <guyavrah1986@gmail.com>
2018-12-17 22:46:19 +0200

    CHAPTER_2: adding the notes file and some initial examples of how we define and set variables within a script.

---
+++ 2_variables/variablesExample.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#########################################################
+# -------------------------------------------------------
+# This script illustrate basic usage of common variables:
+# -------------------------------------------------------
+#########################################################
+
+script_name=$0
+first_arg=$1
+second_arg=$2
+
+echo "script name is:$script_name"
+echo "first arg recived is:$first_arg"
+echo "second arg recived is:$second_arg"
+
+var1=Guy
+var2="Rotem Hasid"
+var3=Avraham
+echo $var1 $var3
+echo "$var2 is calbas"
+
