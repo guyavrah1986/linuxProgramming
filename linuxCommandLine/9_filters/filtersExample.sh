7d13b69f177f24ae3498fce2bcf5e02a5e4d5c2a
Guy Avraham <guyavrah1986@gmail.com>
2018-12-19 13:10:37 +0200

    CHAPTER_9: adding initial commit for usage in filtes.

---
+++ linuxCommandLine/9_filters/filtersExample.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+########################################################################
+# ----------------------------------------------------------------------
+# This script illustrate the basic concept and usage of fiters:
+# ----------------------------------------------------------------------
+########################################################################
+
+echo "==================================="
+echo "$0 - start"
+echo "==================================="
+echo ""
+echo ""
+currDir=$(pwd)
+echo "currently we are at folder:$currDir"
+echo "it contains the following items:"
+ls
+
+fileName=sampleData.txt
+echo "head $fileName"
+head $fileName
+
+echo ""
+echo ""
+
+# `head` example with specified number of lines
+numOfLines=3
+echo "head $numOfLines $fileName"
+head -$numOfLines $fileName
+
+echo ""
+echo ""
+
+# basic `sort` usage (default it sorting alphabetically)
+echo "sort $fileName"
+sort $fileName
+
+echo ""
+echo ""
+
+# basic usage of `wc` with the -w option to return the number of words in the file
+numOfWordsInFile=$(wc -w $fileName)
+echo "$fileName has $numOfWordsInFile in it"
+
+echo ""
+echo ""
+
+# usage of `cut` with delimiter and number of column otions provided
+echo "cut -d ' ' -f 1,2 $fileName"
+cut -d ' ' -f 1,2 $fileName
+
+echo ""
+echo ""
+
+
+
+echo ""
+echo ""
+echo "==================================="
+echo "$0 - end"
+echo "==================================="
