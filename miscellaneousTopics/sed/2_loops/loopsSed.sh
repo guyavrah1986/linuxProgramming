8e9cfa84e22f25a27ae0a6a6b30bf0f1fa9536c0
Guy Avraham <guyavrah1986@gmail.com>
2019-01-04 21:56:33 +0200

    SED: added the loops folder (chapter 2).

--- miscellaneousTopics/sed/2_loops/loopsSed.sh
+++ miscellaneousTopics/sed/2_loops/loopsSed.sh
@@ -20,31 +20,6 @@ echo "${green}===============================${reset}"
 # '<command(s)>': Are the set of "sed commands" that will be invoked on EVERY line in the "pattern buffer".
 # <file_name>: is the name of the file that will "provide" the input (i.e.- STDIN or pipe is also legit as we mentioned).
 fileName=file1.txt
-echo "echo-ing using sed:"
-echo "--------------------"
-sed '' $fileName
-
-echo ""
-echo ""
-
-# in this example we will use the "delete" command of sed
-# note that every "delete" command needs to be prefixed with a '-e' notation
-echo "before applying sed's delete command, $fileName has:"
-echo "----------------------------------------------------"
-cat $fileName
-
-echo "now, we are about to delete the first and third rows"
-sed -e '1d' -e '3d' $fileName
-
-echo ""
-echo ""
-
-# in this example we illustrate the second approach that sed can be invoked - by using a file
-# with sed commands.
-commandsFile=sedCommands.txt
-echo "now we are about to invoke sed with some commands from a file"
-sed -f $commandsFile $fileName
-
 
 echo "${green}============================="
 echo "$0 - end"
--- miscellaneousTopics/sed/README.md
+++ miscellaneousTopics/sed/README.md
@@ -2,6 +2,7 @@
 This sub-folder holds short examples and notes regarding how to use the `sed` utility.
 
 # Structure:
+
 - 1) basic: the "three-phase" work process of `sed`, the two forms to exectute it (commands in line OR with file of commands), notes about the "pattern buffer".
 
-- 
+- 2) loops: 
