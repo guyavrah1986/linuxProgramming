209a96c80ddc8943c34690fcbb4d4159331f0857
Guy Avraham <guyavrah1986@gmail.com>
2019-11-09 09:21:45 +0200

    PROCESSES_AND_JOBS:first and basic example of the job and process concepts.

---
+++ miscellaneousTopics/proccessesAndJobsControl/processesAndJobs.md
@@ -0,0 +1,12 @@
+## Process, process group, jobs and sessions in Linux
+
+# Shell
+Each shell you get at login becomes the leader of its own new session and process group, and sets the controlling process group of the terminal to itself.
+The shell creates a process group within the current session for each "job" it launches, and places each process it starts into the appropriate process group.
+For example, `ls | head` is a pipeline of two `processes`, which the shell considers a single `job`, and will belong to a single, new process group.
+
+# Process
+A process is a (collection of) thread of execution and other context, such as address space and file descriptor table. A process may start other processes; these new processes will belong to the same process group as the parent unless other action is taken. Each process may also have a "controlling terminal", which starts off the same as its parent.
+
+# Jobs
+The shell has the concept of "foreground" jobs and "background" jobs. Foreground jobs are process groups with control of the terminal, and background jobs are process groups without control of the terminal.
---
+++ miscellaneousTopics/proccessesAndJobsControl/processesExample.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#===============================================================================================================================
+#===============================================================================================================================
+# constants
+# --------
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+#===============================================================================================================================
+#===============================================================================================================================
+script_name=$0
+
+echo "${green}===============================${reset}"
+echo "${script_name} - start"
+echo "${green}===============================${reset}"
+
+currDir=$(pwd)
+echo "current working directory is:$currDir"
+background_process_script_name="simpleBackgroundProcess.sh"
+echo "${blue}${script_name} - about to start script:${background_process_script_name} in the background${reset}"
+
+# this command will lunch the provided script in the background, making it:
+# - Within this shell session.
+# - Within this process group.
+# - Its output will NOT be displayed in the "running shell", thus it will NOT "block" the prompt of the running shell.
+./${background_process_script_name} > /dev/null 2>&1 &
+echo "${blue}${script_name} - displaying all jobs in this shell session:${reset}"
+jobs
+
+echo "${green}===============================${reset}"
+echo "${script_name} - end"
+echo "${green}===============================${reset}"
+
+
